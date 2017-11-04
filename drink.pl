2f4f6f6c2fe3406f6e7e73c6f30b7e926f3e170e
Beata Obrok <beata.obrok@u2i.com>
2017-11-04 22:16:23 +0100

    Rules for types of coffee and beer

--- drink.pl
+++ drink.pl
@@ -1,26 +1,48 @@
 :- dynamic
     xpositive/2,
-    xngative/2.
+    xnegative/2.
 
-drink(cofee) :- daytime(morning),
+drink(ice_coffee) :-
+    drink(coffee),
+    weather(warm).
+
+drink(americano) :-
+    drink(coffee),
+    weather(cold).
+
+drink(coffee) :- daytime(morning),
 				tiredness(high).
 
 drink(tea) :- daytime(morning),
 				\+tiredness(high).
 
-drink(juice) :- \+daytime(morning).
+drink(beer) :-
+    daytime(evening),
+    weekend().
+
+drink(juice) :- \+daytime(morning), \+daytime(evening).
 
 tiredness(high) :- positive(tiredness,high).
 
-daytime(morning):- positive(it,morning).			 
+weekend() :- day_of_week(friday); day_of_week(saturday); day_of_week(sunday).
+
+day_of_week(X) :- positive(day_of_week, X).
+
+daytime(morning) :- positive(it, morning).
+
+daytime(evening) :- positive(it, evening).
+
+weather(warm) :- positive(weather, warm).
+
+weather(cold) :- \+weather(warm).
 
 positive(X,Y) :- xpositive(X,Y), !.
 
-positive(X,Y) :- \+xngative(X,Y), ask(X,Y).
+positive(X,Y) :- \+xnegative(X,Y), ask(X,Y).
 
-ngative(X,Y) :- xngative(X,Y), !.
+negative(X,Y) :- xnegative(X,Y), !.
 
-ngative(X,Y) :- \+xpositive(X,Y), ask(X,Y).
+negative(X,Y) :- \+xpositive(X,Y), ask(X,Y).
 
 ask(X,Y) :- !, format('~w is ~w ? (y/n)~n',[X,Y]),
                     read(Reply),
@@ -31,13 +53,15 @@ ask(X,Y) :- !, format('~w is ~w ? (y/n)~n',[X,Y]),
 			    		remember(X,Y,yes),
 					true.
                     
+remember(X, Y) :- assertz(xequals(X, Y)).
+
 remember(X,Y,yes) :- assertz(xpositive(X,Y)).
 
-remember(X,Y,no) :- assertz(xngative(X,Y)).
+remember(X,Y,no) :- assertz(xnegative(X,Y)).
 
 clear_memory:- write('Click something to quit...'), nl,
                     retractall(xpositive(_,_)),
-                    retractall(xngative(_,_)),
+                    retractall(xnegative(_,_)),
                     get_char(_).
                     
 start :- drink(X), !,
@@ -45,4 +69,3 @@ start :- drink(X), !,
             nl, clear_memory.
             
 start :- write('I have no idea what you should drink. Sorry...'), nl.
-            
