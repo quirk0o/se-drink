ec5409d286e6e61ca0899d084942594a0e1ed63f
wgrabis <wgrabis@gmail.com>
2017-11-12 13:43:11 -0800

    Test of jpl

--- src/main/java/application/manager/impl/DrinkManagerImpl.java
+++ src/main/java/application/manager/impl/DrinkManagerImpl.java
@@ -1,7 +1,7 @@
 package application.manager.impl;
 
 import application.manager.DrinkManager;
-import org.jpl7.JPL;
+import org.jpl7.*;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
@@ -17,6 +17,25 @@ public class DrinkManagerImpl implements DrinkManager{
     public DrinkManagerImpl(){
 
         JPL.init();
+
+        Term load = new Compound("consult", new Term[]{new Atom("/home/wg/Documents/se-drink/prolog/test.pl")});
+        Query x = new Query(load);
+        x.open();
+        System.out.println(x.getSolution());
+
+        Term goal = new Compound( "teacher_of", new Term[]{new Atom("aristotle")});
+        Query q = new Query( goal );
+
+        q.open();
+
+        System.out.println(q.getSolution());
+
+        Term goal2 = new Compound( "teacher_of", new Term[]{new Atom("xsaristotle")});
+        Query q2 = new Query( goal2 );
+
+        q2.open();
+
+        System.out.println(q2.getSolution());
     }
 }
 
